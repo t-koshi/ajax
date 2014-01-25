5b0cd90bd3db6ce6af636563c8b624ebfd519e3a
greapflute <greapflutea2@yahoo.co.jp>
2014-01-26 03:09:55 +0900

    second

--- getdata.js
+++ getdata.js
@@ -2,7 +2,7 @@ $(function(){
   console.log($("#jax"));
   $("#jax").bind("click", function(){
     $.ajax({
-      url: "file:///home/greapflute/www/js/ajax/data.html",
+      url: "http://ajaxtest126.herokuapp.com/data.html",
       type: 'GET',
       dataType: 'html',
       success: function(data){
@@ -11,37 +11,3 @@ $(function(){
     });
   });
 });
-
-/*
-jQuery( function() {
-    jQuery( '#jquery-sample-button' ) . toggle(
-        function() {
-            jQuery . ajax( {
-                url: 'jquery-sample-ajax-html.html',
-                success: function( data ) {
-                    jQuery( '#jquery-sample-ajax' ) . html( data );
-                    jQuery( '#jquery-sample-textStatus' ) . text( '読み込み成功' );
-                },
-                error: function( data ) {
-                    jQuery( '#jquery-sample-textStatus' ) . text( '読み込み失敗' );
-                }
-            } );
-        },
-        function() {
-            jQuery( '#jquery-sample-ajax' ) . html( '' );
-            jQuery( '#jquery-sample-textStatus' ) . text( '' );
-        }
-    );
-} );
-/*
-function getdata(){
-  $.ajax({
-    url: 'data.html',
-    type: 'GET',
-    dataType: 'text',
-    success: function(data){
-      $('#ajax').html(data);
-    }
-  });
-}
-*/
